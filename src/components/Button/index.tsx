7731dfa53aa2f200dad223724f7f530fc6742873
Antonio Junior <antoniojrfsousa@gmail.com>
2024-09-05 09:50:38 -0300

    first commit

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,8 @@
+import { ButtonContainer } from "./styles";
+import { IButtonProps } from "./types";
+
+const Button = ({ title}: IButtonProps) => {
+  return <ButtonContainer >{title}</ButtonContainer>;
+};
+
+export default Button;
